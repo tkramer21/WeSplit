696b37a6fe1e70fb35184d786fb77d47def8df6e
tkramer21 <kramertf5@gmail.com>
2023-06-15 13:11:13 -0500

    Created a Check Splitting app with basic functionality. Still need to debug calculation progress, but basic layout is there

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var mPrice = 0.00
+    @State private var mTipPer = 0.2
+    @State private var mNumPeople = 1
+    
+    let maxPeople = 100
+    
+    var body: some View
+    {
+        NavigationView
+        {
+            ZStack
+            {
+                VStack
+                {
+                    Form
+                    {
+                        TextField("Meal Price", value: $mPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                        
+                        Picker(selection: $mNumPeople, label: Text("Number of people"), content: {
+                            ForEach(1...maxPeople, id:\.self)
+                            {
+                                number in
+                                Text("\(number)").tag(number)
+                            }
+                        })
+                        Picker(selection: $mTipPer, label: Text("Tip Percentage"), content:{
+                            ForEach(0 ... 100, id:\.self)
+                            {
+                                number in Text("\(number)").tag(number)
+                            }
+                        })
+                    }.navigationTitle("Check Splitter")
+                    Section
+                    {
+                        Text("Total Price per Person: \(calculateTotalPricePerPerson(), specifier: "%.2f")")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func calculateTotalPricePerPerson() -> Double {
+        let totalPrice = mPrice + mPrice * mTipPer
+        return totalPrice / Double(mNumPeople)
     }
 }
 
