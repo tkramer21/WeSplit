a4c1c2e9c324cb9cb576adf6498ece268e73b007
tkramer21 <kramertf5@gmail.com>
2023-06-15 09:01:39 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kramer, Tim on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
