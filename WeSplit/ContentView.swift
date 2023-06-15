09d5385bde9fe5ee0fa851663d74dcb3f605fbb7
tkramer21 <kramertf5@gmail.com>
2023-06-15 14:43:02 -0500

    Fixed Calculations. Still cannot get background image to show behind forms.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,43 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View
+    {
+        CheckView()
+    }
+}
+    
+    
+    
+struct CheckView: View {
+        
     @State private var mPrice = 0.00
-    @State private var mTipPer = 0.2
+    @State private var mTipPer = 0
     @State private var mNumPeople = 1
     
     let maxPeople = 100
     
     var body: some View
     {
-        NavigationView
-        {
+        GeometryReader
+        { geo in
+        
             ZStack
             {
-                VStack
+                Image("MoneyWings")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .opacity(1.0)
+                
+                NavigationView
                 {
-                    Form
+                    VStack
                     {
-                        TextField("Meal Price", value: $mPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                        Form
+                        {
+                            TextField("Meal Price", value: $mPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                            
+                            Picker(selection: $mNumPeople, label: Text("Number of people"), content: {
+                                ForEach(1...maxPeople, id:\.self)
+                                {
+                                    number in
+                                    Text("\(number)").tag(number)
+                                }
+                            })
+                            Picker(selection: $mTipPer, label: Text("Tip Percentage (%)"), content:{
+                                ForEach(0 ... 100, id:\.self)
+                                {
+                                    number in Text("\(number)").tag(number)
+                                }
+                            })
+                        }.navigationTitle("Check Splitter")
                         
-                        Picker(selection: $mNumPeople, label: Text("Number of people"), content: {
-                            ForEach(1...maxPeople, id:\.self)
-                            {
-                                number in
-                                Text("\(number)").tag(number)
-                            }
-                        })
-                        Picker(selection: $mTipPer, label: Text("Tip Percentage"), content:{
-                            ForEach(0 ... 100, id:\.self)
-                            {
-                                number in Text("\(number)").tag(number)
-                            }
-                        })
-                    }.navigationTitle("Check Splitter")
-                    Section
-                    {
-                        Text("Total Price per Person: \(calculateTotalPricePerPerson(), specifier: "%.2f")")
-                            .font(.system(size: 20))
-                            .fontWeight(.bold)
+                        
+                            Text("Total Price per Person: \(calculateTotalPricePerPerson(), specifier: "%.2f")")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                        
                     }
                 }
             }
@@ -52,7 +73,7 @@ struct ContentView: View {
     }
     
     private func calculateTotalPricePerPerson() -> Double {
-        let totalPrice = mPrice + mPrice * mTipPer
+        let totalPrice = mPrice + mPrice * Double(mTipPer) / 100.0
         return totalPrice / Double(mNumPeople)
     }
 }
